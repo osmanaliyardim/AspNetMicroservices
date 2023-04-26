569f986b4f4d3b6e82f52a1b8873b0a545a0af74
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-04-26 13:05:04 +0300

    Catalog Service
    Catalog Service initilization with mongo data seeding

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement(nameof(Name))]
+        public string Name { get; set; }
+
+        [BsonElement(nameof(Category))]
+        public string Category { get; set; }
+
+        [BsonElement(nameof(Summary))]
+        public string Summary { get; set; }
+
+        [BsonElement(nameof(Description))]
+        public string Description { get; set; }
+
+        [BsonElement(nameof(ImageFile))]
+        public string ImageFile { get; set; }
+
+        [BsonElement(nameof(Price))]
+        public decimal Price { get; set; }
+    }
+}
