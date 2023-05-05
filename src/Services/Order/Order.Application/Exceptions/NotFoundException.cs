8962d6117e6479cdd045ff8c7e8292c404e098e8
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-05-05 06:53:38 +0300

    Order Service
    Order Command features has been created.

---
+++ src/Services/Order/Order.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Order.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+            
+        }
+    }
+}
--- src/Services/Order/Order.Application/Mappings/MappingProfile.cs
+++ src/Services/Order/Order.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
+using Order.Application.Features.Orders.Commands.DeleteOrder;
+using Order.Application.Features.Orders.Commands.UpdateOrder;
 using Order.Application.Features.Orders.Queries.GetOrdersList;
 using Order.Domain.Entities;
 
@@ -9,6 +12,9 @@ namespace Order.Application.Mappings
         public MappingProfile() 
         {
             CreateMap<OrderModel, OrdersVm>().ReverseMap();
+            CreateMap<OrderModel, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<OrderModel, UpdateOrderCommand>().ReverseMap();
+            CreateMap<OrderModel, DeleteOrderCommand>().ReverseMap();
         }
     }
 }
