7740240d5401610d16b7f08c954a5fff5bfe2a0d
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-05-17 09:12:57 +0300

    Elastic Stack Logging
    Common class library has been created for all microservices.

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,3 +1,4 @@
+using Common.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -16,26 +17,7 @@ namespace AspnetRunBasics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, config) =>
-                {
-                    config
-                        .Enrich.FromLogContext()
-                        .Enrich.WithMachineName()
-                        .WriteTo.Console()
-                        .WriteTo.Elasticsearch(
-                            new ElasticsearchSinkOptions(
-                                new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-                            {
-                                IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}" +
-                                $"-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}" +
-                                $"-logs-{DateTime.UtcNow:yyyy-MM}",
-                                AutoRegisterTemplate = true,
-                                NumberOfShards = 2,
-                                NumberOfReplicas = 1,
-                            })
-                        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
-                        .ReadFrom.Configuration(context.Configuration);
-                })
+                .UseSerilog(SeriLogger.Configure)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
